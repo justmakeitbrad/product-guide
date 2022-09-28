f3eeacc303a5277a0d101724e1081d324ff7bf53
justmakeitbrad <bradfordlllovell@gmail.com>
2022-09-27 19:39:24 -0600

    Fleshed out JavaScript with button funtionality

--- scripts.js
+++ scripts.js
@@ -1,39 +1,49 @@
 //add code that changes product code w/ button input//
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_text //
 
-function myFunction() {
-    var x = document.getElementById("myDIV");
+function sclSelector() {
+    var x = document.getElementById("finalCode");
     if (x.innerHTML === "AR101") {
         x.innerHTML = "AR101 SCL";
     } else {
-        x.innerHTML = "AR101";
+        x.innerHTML = "AR101 SCL";
     }
 }
-
-function finishSelector11P() {
-    var x = document.getElementById("myDIV");
+/*
+function kw1Selector() {
+    var x = document.getElementById("finalCode");
+    let finishSlice = str.slice(4)
     if (x.innerHTML === "AR101") {
-        x.innerHTML = "AR101 11P";
-    } else {
         x.innerHTML = "AR101";
+    } else {
+        x.innerHTML = "AR101" + ${finishSlice}; //splice ${} to keep finish
+}
+*/
+function finishSelector11P() {
+    var x = document.getElementById("finalCode");
+    var search = x.search(SCL)
+    if (search.innerHTML == true) {
+        x.innerHTML = x + " 11P";
+    } else {
+        x.innerHTML = "AR101 11P";
     }
 }
 
 function finishSelector15() {
-    var x = document.getElementById("myDIV");
+    var x = document.getElementById("finalCode");
     if (x.innerHTML === "AR101") {
         x.innerHTML = "AR101 15";
     } else {
-        x.innerHTML = "AR101";
+        x.innerHTML = "AR101 15";
     }
 }
 
 function finishSelectorFBL() {
-    var x = document.getElementById("myDIV");
+    var x = document.getElementById("finalCode");
     if (x.innerHTML === "AR101") {
         x.innerHTML = "AR101 FBL";
     } else {
-        x.innerHTML = "AR101";
+        x.innerHTML = "AR101 FBL";
     }
 }
 
